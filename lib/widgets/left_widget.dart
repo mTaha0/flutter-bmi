65aba95eb96ca036d23a9049ff7bd3a9b848baa1
mTaha0 <72344602+mTaha0@users.noreply.github.com>
2021-11-19 21:29:05 +0300

    Update left_widget.dart

--- lib/widgets/left_widget.dart
+++ lib/widgets/left_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class LeftBar extends StatelessWidget {
   final double barWidth;
-  const LeftBar({required Key key, required this.barWidth}) : super(key: key);
+  const LeftBar({Key? key, required this.barWidth}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
